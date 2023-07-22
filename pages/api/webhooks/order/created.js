cd5d61a11c3093644014e79debd6a9e55540c0ec
Michael <tharratt.michael@gmail.com>
2023-07-22 12:40:45 +1000

    experimenting with two seperate email creation functions on the one webook using res.write to send 2 responses in the try catch block

--- pages/api/webhooks/order/created.js
+++ pages/api/webhooks/order/created.js
@@ -9,9 +9,9 @@ import orderCreated from "../../../../src/webhooks/order/created";
 export default async function webhookOrderCreated(req, res) {
   try {
     const [customerEmail, storeEmail] = await orderCreated(req.body);
-    res.status(200).write(customerEmail);
-    res.status(200).write(storeEmail);
-    res.end()
+    res.status(200).send(storeEmail);
+    // res.status(200).write(storeEmail);
+    // res.end()
   } catch (e) {
     console.log("Error at orderCreated webhook");
     console.log(e);
